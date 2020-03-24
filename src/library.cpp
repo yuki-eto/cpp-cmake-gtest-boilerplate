f0ed3e6235bb97da13409429b8d4a83bcff56986
yuki-eto <yuki.eto@toydium.co.jp>
2020-03-24 14:02:19 +0900

    fix for shared lib

--- src/library.cpp
+++ src/library.cpp
@@ -3,3 +3,15 @@
 int sum(int a, int b) {
     return a + b;
 }
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int factorial(int a) {
+    int n = 1;
+    for (int i = 1; i <= a; i++) {
+        n *= i;
+    }
+    return n;
+}
--- test/test_library.cpp
+++ test/test_library.cpp
@@ -4,3 +4,11 @@
 TEST(SumTest, Normal) {
     EXPECT_EQ(10, sum(5, 5));
 }
+
+TEST(MulTest, Noraml) {
+    EXPECT_EQ(20, mul(10, 2));
+}
+
+TEST(FactorialTest, Normal) {
+    EXPECT_EQ(3628800, factorial(10));
+}
